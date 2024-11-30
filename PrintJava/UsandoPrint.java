67eba9ebe60643c77ec2f1339629a1d41ee110d2
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-11-30 15:36:37 -0300

    Update UsandoPrint.java

--- PrintJava/UsandoPrint.java
+++ PrintJava/UsandoPrint.java
@@ -1,5 +1,7 @@
 //Regra número 1 = A classe principal tem que ter o mesmo nome do arquivo
 //Regra 2 = Cada variavel deve receber uma tipagem, ou seja, diga se é float, string ou int
+//println = escreva em linha.
+//Tem sempre que descrever o tipo de dado
 
 public class UsandoPrint {
     public static void main(String[] args) {
