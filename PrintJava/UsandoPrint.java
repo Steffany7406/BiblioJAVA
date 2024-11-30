bf5d857c2a127ecc54f8bb85b18bacbdd063e064
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-11-30 15:35:45 -0300

    Add files via upload

---
+++ PrintJava/UsandoPrint.java
@@ -0,0 +1,8 @@
+//Regra número 1 = A classe principal tem que ter o mesmo nome do arquivo
+//Regra 2 = Cada variavel deve receber uma tipagem, ou seja, diga se é float, string ou int
+
+public class UsandoPrint {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
